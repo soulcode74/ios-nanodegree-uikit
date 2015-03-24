6c4a287297cb78b46f96dd8308bdac717c3038fe
Soulcode74 <soulcode74@gmail.com>
2015-03-25 00:35:23 +0100

    Added HistoryTableViewCell

---
+++ RoshamboWithHistory_incomplete/RockPaperScissors/HistoryTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryTableViewCell.swift
+//  RockPaperScissors
+//
+//  Created by Andrea Perazzi on 24/03/15.
+//  Copyright (c) 2015 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var p1Figure: UIImageView!
+    
+    @IBOutlet var p2Figure: UIImageView!
+    
+}
--- RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
+++ RoshamboWithHistory_incomplete/RockPaperScissors/HistoryViewController.swift
@@ -24,7 +24,7 @@ class HistoryViewController: UIViewController, UITableViewDelegate ,UITableViewD
     }
     
     
-    // Mark: Table View Data Source
+    // Table View Data Source
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.history.count
@@ -32,26 +32,43 @@ class HistoryViewController: UIViewController, UITableViewDelegate ,UITableViewD
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-        var cell = tableView.dequeueReusableCellWithIdentifier("historyCell") as UITableViewCell
+        var cell = tableView.dequeueReusableCellWithIdentifier("historyCell") as HistoryTableViewCell
         var match = self.history[indexPath.row]
         
-        cell.textLabel?.text = self.resultString(match)
-        cell.detailTextLabel?.text = "\(match.p1) vs \(match.p2)"
-        
+        cell.titleLabel.text = self.resultString(match)
+        cell.p1Figure.image = self.imageForMatch(match.p1)
+        cell.p2Figure.image = self.imageForMatch(match.p2)
+
         return cell
     }
     
     
     // Result string function
     func resultString(match: RPSMatch) -> String {
-        
         // Handle the tie
         if match.p1 == match.p2 {
             return "It's a tie!"
         } else {
             return match.p1.defeats(match.p2) ? "You Win!" : "You Lose!"
         }
-
+    }
+    
+    
+    // Image gesture
+    func imageForMatch(gesture: RPS) -> UIImage {
+        
+        var name = ""
+        
+        switch (gesture) {
+        case .Rock:
+            name = "rock"
+        case .Paper:
+            name = "paper"
+        case .Scissors:
+            name = "scissors"
+        }
+        
+        return UIImage(named: name)!
     }
     
     
